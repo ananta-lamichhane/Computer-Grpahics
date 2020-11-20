063668372424ab72e47f7e2b361ab637ccb87614
ananta-lamichhane <bloomsnruins@gmail.com>
2020-11-20 18:16:56 +0100

    Added new functioalitities.. some stuff still to do.

--- 1-Affine-Matrix-Transformations/cg1_exercise_1/skeleton/src/main.ts
+++ 1-Affine-Matrix-Transformations/cg1_exercise_1/skeleton/src/main.ts
@@ -20,56 +20,82 @@ function rotateUsingMatrix(a, b, g, objMatrix){ // rotation along z, y and x res
     return objMatrix.multiply(trMatrix); // multiply the transformation matrix of the object with the matrix we got from parameter.
 }
 
+function moveObject(x,y,z, obj){
+    const trMatrix = new THREE.Matrix4();
+    trMatrix.set(1,0,0,x,
+                    0,1,0,y,
+                    0, 0, 1, z,
+                    0,0,0,1);
+    obj.matrix.multiply(trMatrix);
+    return obj;
+}
+
+function moveObject2(x,y,z, obj){
+    const trMatrix = new THREE.Matrix4();
+    trMatrix.set(1,0,0,x,
+        0,1,0,y,
+        0, 0, 1, z,
+        0,0,0,1);
+    obj.geometry.matrixWorld.multiply(trMatrix);
+    return obj;
+}
+
 function highlightChild(curr){
     //curr.material.color.set('red');
-    var ret;
     var child;
-    for(var i=0; i<curr.children.length; i++){
-        if(curr.children[i] instanceof  THREE.Mesh){
+    for(var i=0; i<curr.children.length; i++) {
+        if (curr.children[i] instanceof THREE.Mesh) {
             child = curr.children[i]; // find the first child of type Mesh.
-            console.log(child);
+            //console.log(child);
             (curr.children[i]).material.color.set('red'); // color its paretn green and it red (highlight)
             child.parent.material.color.set('green'); // " remove highlight"
-            break;
+            return child;
         }
-        window.addEventListener("keydown", event => { // do highlighting recursively
-            if(event.key == "s"){
-               child = highlightChild(child);
-            }
-        });
     }
-    return child;
+    return curr;
+
 }
 
 function selectNextSibling(curr){ // find next sibling which is Mesh, cycle through if not found
     const siblings = curr.parent.children;
     var i = siblings.indexOf(curr);
-    for(var j=i; j< siblings.length; j++){
-        console.log("j = " + j + " i= "+i + "len = "+ siblings.length);
-        if(j == siblings.length -1){
-            j= -1;
+    if(siblings.length <= 1){
+        return curr;
+    }else {
+        for (var j = i; j < siblings.length; j++) {
+           // console.log("j = " + j + " i= " + i + "len = " + siblings.length);
+            if (j == siblings.length - 1) {
+                j = -1;
+            }
+            if (siblings[j + 1] instanceof THREE.Mesh) {
+                curr.children.forEach(curr.children.material.color.set('green'));
+                siblings[j + 1].material.color.set('red');
+                return siblings[j + 1];
+            }
         }
-        if(siblings[j+1] instanceof THREE.Mesh){
-            curr.material.color.set('green');
-            siblings[j+1].material.color.set('red');
-            return siblings[j+1];
-        }
+        return curr;
     }
 }
+
 
 function selectPrevSibling(curr){//find previous sibling which is Mesh, cycle through if not found
     const siblings = curr.parent.children;
     var i = siblings.indexOf(curr);
-    for(var j=i; j>0; j--){
-        console.log("j = " + j + " i= "+i + "len = "+ siblings.length);
-        if(j == 1){
-            j= siblings.length;
+    if(siblings.length == 1){
+        return curr;
+    }else {
+        for (var j = i; j > 0; j--) {
+            console.log("j = " + j + " i= " + i + "len = " + siblings.length);
+            if (j == 1) {
+                j = siblings.length;
+            }
+            if (siblings[j - 1] instanceof THREE.Group) {
+                curr.traverse(elem => elem.material.color.set("blue"));//curr.material.color.set('green');
+                //siblings[j - 1].material.color.set('red');
+                return siblings[j - 1];
+            }
         }
-        if(siblings[j-1] instanceof THREE.Mesh){
-            curr.material.color.set('green');
-            siblings[j-1].material.color.set('red');
-            return siblings[j-1];
-        }
+        return curr;
     }
 }
 
@@ -79,8 +105,33 @@ function highlightParent(curr){ // go to parent, highlight if of type mesh (nest
         curr.parent.material.color.set('red');
         return(curr.parent);
     }else{
+        curr.material.color.set('red');
         return curr;
     }
+}
+
+
+function changeRotCenter(obj, vec, rad){ // change axis origin to arbitrary point
+    console.log("what the hell");
+    var orig = obj.geometry.computeBoundingBox();
+    console.log("now this");
+    console.log(orig);
+}
+
+function showHideAxes(obj){
+    for(var i=0; i<obj.children.length; i++){
+        if(obj.children[i] instanceof THREE.AxesHelper){
+            obj.children[i].material.visible = obj.children[i].material.visible == false;
+        }
+    }
+}
+
+function moveObjectEx(x, y, z, obj){
+    for(var i=0; i< obj.geometry.vertices.length; i++){
+        obj.geometry.vertices[i].add(new THREE.Vector3(x,y,z));
+    }
+
+    return obj;
 }
 
 function main(){
@@ -110,32 +161,110 @@ function main(){
     var material = new THREE.MeshLambertMaterial({color:'green'});
     var cube = new THREE.Mesh(geometry, material);
 
-    var geometry2 = new THREE.BoxGeometry(0.5,0.1,0.1);
+    var geometry2 = new THREE.BoxGeometry(0.2,0.1,0.1);
     var material2 = new THREE.MeshLambertMaterial({color:'green'});
     var cube2 = new THREE.Mesh(geometry2, material2);
 
-    var geometry2_1 = new THREE.BoxGeometry(0.9,0.3,0.1);
+    var geometry2_1 = new THREE.BoxGeometry(0.4,0.1,0.1);
     var material2_1 = new THREE.MeshLambertMaterial({color:'green'});
     var cube2_1 = new THREE.Mesh(geometry2_1, material2_1);
 
-    var geometry3 = new THREE.BoxGeometry(0.2,0.6,0.1);
+    var geometry2_2 = new THREE.BoxGeometry(0.4,0.1,0.1);
+    var material2_2 = new THREE.MeshLambertMaterial({color:'green'});
+    var cube2_2 = new THREE.Mesh(geometry2_2, material2_2);
+
+    var grp0 = new THREE.Group();
+
+    var grp5 = new THREE.Group();
+    var grp3 = new THREE.Group();
+    var grp2 = new THREE.Group();
+    var grp1 = new THREE.Group();
+    var grp4 = new THREE.Group();
+
+
+
+    var geometry3 = new THREE.BoxGeometry(0.06,0.3,0.1);
     var material3 = new THREE.MeshLambertMaterial({color:'green'});
     var cube3 = new THREE.Mesh(geometry3, material3);
+
+    var geometry3_1 = new THREE.BoxGeometry(0.06,0.3,0.1);
+    var material3_1 = new THREE.MeshLambertMaterial({color:'green'});
+    var cube3_1 = new THREE.Mesh(geometry3_1, material3_1);
+
+    var geometry4 = new THREE.BoxGeometry(0.05,0.05,0.1);
+    var material4 = new THREE.MeshLambertMaterial({color:'white'});
+    var cube4 = new THREE.Mesh(geometry4, material4);
+
+    var geometry4_1 = new THREE.BoxGeometry(0.05,0.05,0.1);
+    var material4_1 = new THREE.MeshLambertMaterial({color:'white'});
+    var cube4_1 = new THREE.Mesh(geometry4_1, material4_1);
 
 
     const axesHelper = new THREE.AxesHelper(5);
     const axesHelper2 = new THREE.AxesHelper(2);
-    cube.add(axesHelper);
-    cube.add(cube2);
-    cube.add(cube2_1);
-    cube2.add(axesHelper2);
-    cube2.add(cube3);
+
+
+    grp1.matrixAutoUpdate = false;
+    grp2.matrixAutoUpdate = false;
+    grp3.matrixAutoUpdate = false;
+    grp4.matrixAutoUpdate = false;
+    grp5.matrixAutoUpdate = false;
+    grp0.matrixAutoUpdate = false;
+
 
     cube.matrixAutoUpdate = false;
     cube2.matrixAutoUpdate = false;
     cube2_1.matrixAutoUpdate = false;
     cube3.matrixAutoUpdate = false;
-    scene.add(cube);
+    cube2_2.matrixAutoUpdate = false;
+    cube3_1.matrixAutoUpdate = false;
+    cube4.matrixAutoUpdate = false;
+    cube4_1.matrixAutoUpdate = false;
+
+
+    grp1.add(cube);
+    grp1.add(axesHelper);
+    var vec = new THREE.Vector3(1,1,1);
+
+   // cube2 = moveObject(0, 0.4,0, cube2);
+    grp2 = moveObject(0, 0.4,0, grp2);
+    grp2.add(cube2);
+
+
+    grp3 = moveObject(-0.05, -0.45, 0,grp3);
+    grp3.add(cube3);
+
+
+    console.log("---------------------------");
+
+    console.log(cube.geometry.vertices);
+    console.log("---------------------------")
+
+    grp0.add(grp1);
+    grp0.add(grp2);
+    grp0.add(grp3);
+
+
+
+    //cube2_2 = moveObject(-0.4, 0, 0, cube2_2);
+   // cube3 = moveObject(-0.05, -0.45, 0, cube3);
+    //cube3.geometry.center();
+   // cube3_1 = moveObject(0.05, -0.45, 0, cube3_1);
+
+   // cube.add(cube2_1);
+    //cube2_1 = moveObject(0.4, 0, 0, cube2_1);
+    //cube.add(cube2_2);
+   // cube2.add(axesHelper2);
+    //cube.add(cube3);
+    //scube3.add(new THREE.AxesHelper(0.2));
+    //cube3.add(cube4);
+    //cube3_1.add(cube4_1);
+    //cube4_1.matrix = moveObject(0.1, 0.1, 0, cube4_1.matrix);
+    //cube4.matrix = moveObject(0.1, 0.1, 0, cube4.matrix);
+   // cube.add(cube3_1);
+
+    const orig = grp0;
+    scene.add(grp0);
 
     /*----------------------------------------keyboard Input --------------------------------------------*/
 var curr = cube;
@@ -162,11 +291,8 @@ var curr = cube;
 
         }
         if (event.key == "s") {
-            if(curr.children.length < 1 ){
-                alert("reached maximum depth of object");
-            }else {
+
                 curr = highlightChild(curr);
-            }
         }
 
         if(event.key == "d"){
@@ -178,7 +304,18 @@ var curr = cube;
         if(event.key == "w"){
             curr = highlightParent(curr);
         }
-
+        if(event.key == "w"){
+            curr = orig;
+        }
+        if(event.key == "c"){
+            showHideAxes(curr);
+        }
+        if(event.key == "x"){
+            var s= new THREE.Sphere();
+           // curr.geometry.normalize();
+            curr.geometry.boundingSphere?.set(new THREE.Vector3(0,0,0), 2);
+            curr.geometry.center();
+        }
     });
 
     /*-------------------------- Lights, camera , Render.... ------------------------------------------------*/
